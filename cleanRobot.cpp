754690791ebb7d0a0a75b8e8d9fe55e1f27ccc1c
YiHung Liu <roger_joker@livemail.tw>
2018-11-19 01:06:16 +0800

    project start

--- cleanRobot.cpp
+++ cleanRobot.cpp
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 class node{
     int row;
@@ -10,12 +15,31 @@ void createGraph(void){
     return;
 }
 
-int main(void){
+int main(int argc, char *argv[]){
+
+    //*****file input*****
+    string studentNum = argv[1];
+    string filePath = ".\\" + studentNum + "\\matrix.data";
+
+    ifstream mapFile;
+    mapFile.open(filePath);
+
+    //*****transfer matrix to graph*****
+
+
 
     createGraph();
 
+    //*****path finding alogorithm*****
 
+    
 
+    //*****file output*****
+    ofstream pathFile;
+    string outputFilePath = ".\\" + studentNum + "\\peak.final";
+    pathFile.open(outputFilePath);
+    pathFile << endl; //output anwser
+    pathFile.close();
 
     return 0;
 }
