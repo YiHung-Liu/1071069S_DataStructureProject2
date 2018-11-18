0c1d5f1e31090036d396e80ec1ad917e854fea19
YiHung Liu <roger_joker@livemail.tw>
2018-11-18 23:57:38 +0800

    github test

---
+++ cleanRobot.cpp
@@ -0,0 +1,21 @@
+
+class node{
+    int row;
+    int col;
+    int dist; //distance to root
+};
+
+void createGraph(void){
+
+    return;
+}
+
+int main(void){
+
+    createGraph();
+
+
+
+
+    return 0;
+}
