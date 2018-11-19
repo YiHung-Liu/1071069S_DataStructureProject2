b947b7c5a77e24771b55be06259b72324f4c28de
YiHung Liu <roger_joker@livemail.tw>
2018-11-20 00:59:55 +0800

    inputFile part1

--- cleanRobot.cpp
+++ cleanRobot.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <string.h>
+#include <vector>
 
 using namespace std;
 
@@ -17,12 +19,36 @@ void createGraph(void){
 
 int main(int argc, char *argv[]){
 
+    int row=0, col=0, battery=0;
+
+
     //*****file input*****
     string studentNum = argv[1];
     string filePath = ".\\" + studentNum + "\\matrix.data";
+    string firstLine; //get matrix info: row, col, battery
+    string matrixString; //get matrix
+
+    vector< vector<int> > matrix(row);
 
     ifstream mapFile;
     mapFile.open(filePath);
+
+    if (mapFile.is_open()){
+        for (int i=0; i<3; i++){
+            getline(mapFile, firstLine, ' ');
+            if (i = 0) row = stoi(firstLine);
+            else if (i = 1) col = stoi(firstLine);
+            else battery = stoi(firstLine);
+        }
+
+        while (getline(mapFile, matrixString)){
+
+        }
+    }
+    else {
+        cerr << "unable to open input file";
+        return -1;
+    }
 
     //*****transfer matrix to graph*****
 
