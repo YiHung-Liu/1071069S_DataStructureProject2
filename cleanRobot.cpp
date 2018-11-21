de74bcc099b77226c9ed0dde125bf766067c4fc4
YiHung Liu <roger_joker@livemail.tw>
2018-11-22 01:38:42 +0800

    add the structure for graph creating

--- cleanRobot.cpp
+++ cleanRobot.cpp
@@ -1,39 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 #include <vector>
 
 using namespace std;
 
-class node{
+struct AdjListNode{
+    int dest;
+    int weight;
+    struct AdjListNode* next;
+};
+
+class Node{
     int row;
     int col;
     int dist; //distance to root
 };
-
-void createGraph(void){
-
-    return;
-}
 
 int main(int argc, char *argv[]){
 
     int row=0, col=0, battery=0;
 
 
-    //*****file input*****
+    //*****file input & creat graph*****
     string studentNum = argv[1];
     string filePath = ".\\" + studentNum + "\\matrix.data";
     string firstLine; //get matrix info: row, col, battery
     string matrixString; //get matrix
 
-    vector< vector<int> > matrix(row);
-
     ifstream mapFile;
     mapFile.open(filePath);
 
     if (mapFile.is_open()){
+
         for (int i=0; i<3; i++){
             getline(mapFile, firstLine, ' ');
             if (i = 0) row = stoi(firstLine);
@@ -41,20 +43,56 @@ int main(int argc, char *argv[]){
             else battery = stoi(firstLine);
         }
 
-        while (getline(mapFile, matrixString)){
+        string *lineRecord = new string[col]; //record the line after visited
+        bool isFirstLine = true;
 
+        while (getline(mapFile, matrixString)){
+            istringstream ss(matrixString);
+            string token;
+
+            int index = 0; //index of lineRecord
+            bool isFirstToken = true;
+
+            while (getline(ss, token, ' ')){
+
+                if (token == "R" || stoi(token) == 0 ){ //paths to go
+
+                    //add Node into adjList
+                    //record: row, col, nodeNumber
+                    //**********************
+
+                    if (isFirstToken){
+                        isFirstToken = false;
+                    }
+                    else if (stoi(lineRecord[index-1]) == 0 || lineRecord[index] == "R"){ //if predeccessor == 0 -> is connected
+                        //add Edge to graph
+                        //
+                        //*****************
+                    }
+
+                    if (isFirstLine){ //first line doesn't need to compare with upper line
+                        isFirstLine = false;
+                    }
+                    else if (stoi(lineRecord[index]) == 0 || lineRecord[index] == "R"){ //second line after
+                        //add Edge to graph
+                        //
+                        //*****************
+                    }
+                }
+                else{ //walls
+                    //do nothing
+                }
+
+                lineRecord[index] = token;
+                index++;
+            }
         }
+        delete []lineRecord;
     }
     else {
         cerr << "unable to open input file";
         return -1;
     }
-
-    //*****transfer matrix to graph*****
-
-
-
-    createGraph();
 
     //*****path finding alogorithm*****
 
